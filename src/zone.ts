ce472ff0e537a35fd8758de10a1bd00b9d7102ac
Ted Cook <teodoro.cook@gmail.com>
2023-09-17 22:05:40 -0600

    Refactor zoneInfo method into external package

--- src/zone.ts
+++ src/zone.ts
@@ -1,24 +1,10 @@
 import { Command } from 'commander';
-import { api } from '@nephelaiio/cloudflare-api';
+import { zoneInfo } from '@nephelaiio/cloudflare-api';
 
 import { apiToken } from './environment';
 import { attempt } from './utils';
 
-async function zoneList(zone: string | null = null) {
-  const token = apiToken();
-  const zones = await api({ token, path: '/zones?per_page=50' });
-  if (zone) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return zones.result.filter((z: any) => z.name === zone);
-  }
-  return zones.result;
-}
-
-async function zoneInfo(zone: string) {
-  const token = apiToken();
-  const zones = await api({ token, path: `/zones?name=${zone}` });
-  return zones.result;
-}
+const token = apiToken();
 
 const zone = (program: Command) => {
   const command = program.command('zone');
@@ -26,7 +12,7 @@ const zone = (program: Command) => {
   command.command('list').action(
     async (_) =>
       await attempt(async () => {
-        const zones = await zoneList();
+        const zones = await zoneInfo({ token });
         console.log(JSON.stringify(zones));
       })
   );
@@ -37,7 +23,7 @@ const zone = (program: Command) => {
     .action(
       async (zone) =>
         await attempt(async () => {
-          const zones = await zoneInfo(zone);
+          const zones = await zoneInfo({ token, zone });
           console.log(JSON.stringify(zones));
         })
     );
